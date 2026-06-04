3ffd8058c96725367da07b820783cf2a84faa65f
DiermeierWolfgang <wolfgang.diermeier@outlook.com>
2026-06-05 01:28:18 +0200

    Deepsleep configured and calibration via home assistant possible during initial startup

--- ESP32C6_ArduinoIDE/BeeHiveLive/BeeHiveLive.ino
+++ ESP32C6_ArduinoIDE/BeeHiveLive/BeeHiveLive.ino
@@ -7,6 +7,7 @@
 #include <OneWireNg_CurrentPlatform.h>
 #include <HX711.h>
 #include <Zigbee.h>
+#include <Preferences.h>
 
 // Input/Output definition
 #define BATTERY_VOLTAGE_PIN A2
@@ -26,9 +27,9 @@
 #define HX711_SENSOR_ENDPOINT_NUMBER          5
 #define CN3791_CHRG_ENDPOINT_NUMBER           6
 #define CN3791_DONE_ENDPOINT_NUMBER           7
-#define DEEPSLEEP_ENDPOINT_NUMBER             8
-#define HX711_GAIN_ENDPOINT_NUMBER            9
-#define HX711_OFFSET_ENDPOINT_NUMBER          10
+#define HX711_GAIN_ENDPOINT_NUMBER            8
+#define HX711_OFFSET_ENDPOINT_NUMBER          9
+#define DEEPSLEEP_DURATION_ENDPOINT_NUMBER    10
 
 // 1-Wire definitions
 #define ONEWIRE_CMD_MATCH_ROM     0x55
@@ -51,9 +52,9 @@ static const int DS18B20_AMOUNT = sizeof(DS18B20) / sizeof(DS18B20[0]);
 
 // Optional: Define names for the temperature sensors
 static const char* DS18B20_NAME[] = {
-    "BeeHive_1",
-    "BeeHive_2",
-    "BeeHive_3",
+    "Temp1",
+    "Temp2",
+    "Temp3",
 };
 
 
@@ -66,17 +67,13 @@ ZigbeeTempSensor zbDS18B20TempSensor[DS18B20_AMOUNT] = {
 ZigbeeAnalog zbHX711Sensor = ZigbeeAnalog(HX711_SENSOR_ENDPOINT_NUMBER);
 ZigbeeAnalog zbHX711Gain = ZigbeeAnalog(HX711_GAIN_ENDPOINT_NUMBER);
 ZigbeeAnalog zbHX711Offset = ZigbeeAnalog(HX711_OFFSET_ENDPOINT_NUMBER);
+ZigbeeAnalog zbDeepSleep = ZigbeeAnalog(DEEPSLEEP_DURATION_ENDPOINT_NUMBER);
 ZigbeeBinary zbCN3791Chrg = ZigbeeBinary(CN3791_CHRG_ENDPOINT_NUMBER);
 ZigbeeBinary zbCN3791Done = ZigbeeBinary(CN3791_DONE_ENDPOINT_NUMBER);
-ZigbeeBinary zbDeepSleep = ZigbeeBinary(DEEPSLEEP_ENDPOINT_NUMBER);
 
 OneWireNg_CurrentPlatform oneWire(ONE_WIRE_BUS_PIN, true);
 HX711 hx711;
-
-// Global variable definition
-float hx711gain;
-float hx711offset;
-bool deepSleep;
+Preferences preferences;
 
 
 void initInternalTempSensor() {
@@ -88,6 +85,8 @@ void initInternalTempSensor() {
   zbInternalTempSensor.setMinMaxValue(10, 50);
   // Optional: Set default (initial) value for the temperature sensor to 10.0°C to match the minimum temperature measurement value
   zbInternalTempSensor.setDefaultValue(10.0);
+  // Set the tolerance
+  zbInternalTempSensor.setTolerance(0.01);
   // Register end point
   Zigbee.addEndpoint(&zbInternalTempSensor);
 }
@@ -102,6 +101,8 @@ void initDS18B20() {
     zbDS18B20TempSensor[i].setMinMaxValue(-40, 85);
     // Optional: Set default (initial) value for the temperature sensor to 20.0°C
     zbDS18B20TempSensor[i].setDefaultValue(20.0);
+    // Set the tolerance
+    zbDS18B20TempSensor[i].setTolerance(0.01);
     // Register end points
     Zigbee.addEndpoint(&zbDS18B20TempSensor[i]);
   }
@@ -123,16 +124,10 @@ void initHX711() {
   zbHX711Sensor.setAnalogInputApplication(0xffffff);
   zbHX711Gain.setAnalogOutputApplication(0xffffff);
   zbHX711Offset.setAnalogOutputApplication(0xffffff);
-  // raw sensor output resolution is 1
+  // Setting appropiate resolutions for the analog values
   zbHX711Sensor.setAnalogInputResolution(0.001);
   zbHX711Gain.setAnalogOutputResolution(0.00001);
   zbHX711Offset.setAnalogOutputResolution(0.001);
-  // Set default values
-  //zbHX711Gain.setAnalogOutput(0.00004);
-  //zbHX711Offset.setAnalogOutput(1.647);
-  // If analog output cluster is added, set callback function for analog output change
-  zbHX711Gain.onAnalogOutputChange(setHX711Gain);
-  zbHX711Gain.onAnalogOutputChange(setHX711Offset);
   // Register end points
   Zigbee.addEndpoint(&zbHX711Sensor);
   Zigbee.addEndpoint(&zbHX711Gain);
@@ -146,29 +141,39 @@ void initBinarySensor() {
   // Create new binary zigbee endpoints
   zbCN3791Chrg.addBinaryInput();
   zbCN3791Done.addBinaryInput();
-  zbDeepSleep.addBinaryOutput();
   // Set a suited application
   zbCN3791Chrg.setBinaryInputApplication(0x00000000);
   zbCN3791Done.setBinaryInputApplication(0x00000000);
-  zbDeepSleep.setBinaryInputApplication(0x00000000);
   // Define the name for each binary sensor
   zbCN3791Chrg.setBinaryInputDescription("Charging");
   zbCN3791Done.setBinaryInputDescription("Charged");
-  zbDeepSleep.setBinaryInputDescription("Deep Sleep");
-  // Callback function on binary change
-  zbDeepSleep.onBinaryOutputChange(enableDeepSleep);
   // Register end points
   Zigbee.addEndpoint(&zbCN3791Chrg);
   Zigbee.addEndpoint(&zbCN3791Done);
+}
+
+void initDeepSleep() {
+  // Create a new analog zigbee endpoint for the deep sleep duration
+  zbDeepSleep.addAnalogOutput();
+  // Define the name of the reading (shows up as friendly name in Home Assistant)
+  zbDeepSleep.setAnalogOutputDescription("Deep Sleep Duration");
+  // Set an unidentified Application to prevent units from showing up in Home Assistant (kg is not avaliable)
+  zbDeepSleep.setAnalogOutputApplication(0xffffff);
+  // Lowest Sleep Duration is 1 sec
+  zbDeepSleep.setAnalogOutputResolution(1);
+  // Register end points
   Zigbee.addEndpoint(&zbDeepSleep);
+  // Deep Sleep Settings
+  esp_zb_sleep_enable(true);
+  esp_zb_sleep_set_threshold(3000);
+  esp_zb_set_rx_on_when_idle(false);
 }
 
 
 // Internal Temperature Sensor
 void internal_temp_sensor_value_update() {
   float tsens_value = temperatureRead();
-  //Serial.printf("Internal → %.4f °C", tsens_value);
-  //Serial.println();
+  Serial.printf("Int. Temperature:     Temp:  %.2f °C\n", tsens_value);
   zbInternalTempSensor.setTemperature(tsens_value);
   zbInternalTempSensor.reportTemperature();
 }
@@ -181,16 +186,15 @@ void soc_sensor_value_update() {
   float soc = (voltage - 3.0f) * (100.0f / (4.2f - 3.0f));              // Li-Ion 3.0–4.2V → SoC 0–100%
   soc = constrain(soc, 0.0f, 100.0f);                                   // Limit from 0% to 100%
 
-  //Serial.printf("Battery ADC=%d Voltage=%.2fV SoC=%.1f%%\r", adc_value, voltage, soc);
-  //Serial.println();
+  Serial.printf("Battery:              ADC: %d Voltage: %.2fV SoC: %.1f%%\r\n", adc_value, voltage, soc);
 
   // Battery information is placed into first endpoint
   zbInternalTempSensor.setBatteryPercentage((uint8_t) soc);
-  //zbInternalTempSensor.setBatteryVoltage((uint8_t) voltage * 10);
+  zbInternalTempSensor.setBatteryVoltage((uint8_t) voltage * 10);
   zbInternalTempSensor.reportBatteryPercentage();
 }
 
-// Temporary function to read the address of one single connected one wire device
+/* Temporary function to read the address of one single connected one wire device
 void discoverOneWire() {
   uint8_t oneWireAddr[8];
   oneWire.searchReset();
@@ -204,7 +208,7 @@ void discoverOneWire() {
     Serial.printf("%x ", oneWireAddr[i]);
   }
   Serial.println();
-}
+}*/
 
 bool readSingleOneWireTemp(int index, float *temp_out)
 {
@@ -230,15 +234,17 @@ void readAllOneWireTemps(float *tsens_value) {
     if (oneWire.addressAll() == OneWireNg::EC_SUCCESS) {          // Trigger broadcast conversion off all connected DS18B20 sensors
       oneWire.writeByte(DS18B20_CMD_CONVERT_T);                   
       delay(DS18B20_CONVERT_MS);
+      Serial.print("Ext. Temperature:     ");
       for (int j = 0; j < DS18B20_AMOUNT; j++) {                  // Loop through all known temperature sensor addresses
         float temp;
         if (readSingleOneWireTemp(j, &temp)) {                    // Read individual sensor
-            //printf("%-10s → %.4f °C\n", DS18B20_NAME[j], temp);   // Print value on serial monitor if reading is valid
+            printf("%s: %.2f°C ", DS18B20_NAME[j], temp);   // Print value on serial monitor if reading is valid
             tsens_value[j] = temp;                                // Store value in correspondind index j for the sensor
         } else {
             printf("%-10s → READ FAILED\n", DS18B20_NAME[j]);     // Print failure on serial monitor
         }
       }
+      Serial.println();
       break;                                                      // Leave for loop after successful broadcast conversion
     }
     delay(100);                                                   // Wait before retry broadcast conversion
@@ -255,20 +261,23 @@ void DS18B20_temp_sensor_value_update() {
   }
 }
 
-void setHX711Gain(float analog_output) {
-  hx711gain = analog_output;
-}
-
-void setHX711Offset(float analog_output) {
-  hx711offset = analog_output;
-}
-
 void hx711_sensor_value_update() {
-  // Use default values if gain is not valid
+  // Get calibration data
+  float hx711gain = zbHX711Gain.getAnalogOutput();
+  float hx711offset = zbHX711Offset.getAnalogOutput();
+  Serial.printf("HX711 Gain:           Home Assistant: %.5f NVM: %.5f\n", hx711gain, preferences.getFloat("hx711gain",0));
+  Serial.printf("HX711 Offset:         Home Assistant: %.3f   NVM: %.3f\n", hx711offset, preferences.getFloat("hx711offset",0));
+
   if (hx711gain == 0) {
-    hx711gain = 0.00004;
-    hx711offset = 1.647;
+    // Use default values or from memory if gain is not valid
+    hx711gain = preferences.getFloat("hx711gain", 0.00004);
+    hx711offset = preferences.getFloat("hx711offset", 1.647);
+  } else {
+    // Udate memory with new values
+    preferences.putFloat("hx711gain", hx711gain);
+    preferences.putFloat("hx711offset", hx711offset);
   }
+
   // Timeout data retrieval after 10 tries
   for (int i = 0; i < 10; i++) {
     if (hx711.is_ready()) {
@@ -277,15 +286,15 @@ void hx711_sensor_value_update() {
       float weight = raw_value * hx711gain + hx711offset;
       // power down the IC to safe battery life
       hx711.power_down();
+      zbHX711Sensor.setAnalogInput(weight);
+      zbHX711Sensor.reportAnalogInput();
 
-      Serial.printf("HX711 reading: raw %f gain %f offset %f weight %f", raw_value, hx711gain, hx711offset, weight);
+      Serial.printf("HX711 reading:        ADC: %.0f               Weight: %.3fkg", raw_value, weight);
       Serial.println();
 
-      zbHX711Sensor.setAnalogInput(weight);
-      //Serial.printf("HX711 Report: %d\n", zbHX711Sensor.reportAnalogInput());
       break;
     } else {
-      Serial.printf("Retry%d: HX711 is not ready\n", i+1);
+      Serial.printf("Retry %d: HX711 is not ready\n", i+1);
       delay(10);
     }
   }
@@ -296,8 +305,7 @@ void cn3791_status_update() {
   bool cn3791_chrg = !(bool)digitalRead(CN3791_CHRG_PIN);
   bool cn3791_done = !(bool)digitalRead(CN3791_DONE_PIN);
 
-  //Serial.printf("Charging State: %d\n", cn3791_chrg);
-  //Serial.printf("Charged State: %d\n", cn3791_done);
+  Serial.printf("CN3791 Status:        Charging: %d             Charged: %d\n", cn3791_chrg, cn3791_done);
 
   // Write pin status to the zigbee binary inputs and report via zigbee
   zbCN3791Chrg.setBinaryInput(cn3791_chrg);
@@ -306,9 +314,29 @@ void cn3791_status_update() {
   zbCN3791Done.reportBinaryInput();
 }
 
-void enableDeepSleep(bool state) {
-  deepSleep = state;
+void enterDeepSleep() {
+  // Get sleep time from home assistant and limit to 1 week
+  uint deepSleepTime = constrain(zbDeepSleep.getAnalogOutput(), 0, 604800);
+  Serial.printf("DeepSleep Duration:   Home Assistant: %d       NVM: %d\n", deepSleepTime, preferences.getUInt("dsTime",0));
+
+  // Store valid duration from home assistant into memory
+  if (deepSleepTime != 0) {
+    preferences.putUInt("dsTime", deepSleepTime);
+  }
+  // Get sleep duration back from memory -> If nothing is stored function will return 0
+  deepSleepTime = preferences.getUInt("dsTime",0);
+  // Set deep sleep time and only go to sleep if a time larger 0 is defined
+  if (deepSleepTime != 0){
+    esp_sleep_enable_timer_wakeup(deepSleepTime * 1000000ULL);
+    preferences.end();
+    Serial.println("Stopping Zigbee");
+    Zigbee.stop();
+    Serial.println("Entering Deep Sleep");
+    Serial.flush();
+    esp_deep_sleep_start();
+  }
 }
+
 
 
 /********************* Arduino functions **************************/
@@ -318,49 +346,41 @@ void setup() {
   // Init button switch
   pinMode(BOOT_PIN, INPUT_PULLUP);
 
-  // Checking button for factory reset
-  if (digitalRead(BOOT_PIN) == LOW) {  // Push button pressed
-    // Key debounce handling
-    delay(100);
-    int startTime = millis();
-    while (digitalRead(BOOT_PIN) == LOW) {
-      delay(50);
-      if ((millis() - startTime) > 3000) {
-        // If key pressed for more than 3secs, factory reset Zigbee and reboot
-        Serial.println("Resetting Zigbee to factory and rebooting in 1s.");
-        delay(1000);
-        Zigbee.factoryReset();
-      }
-    }
-  }
-
-  initInternalTempSensor();           // Initialize internal temperature reading of the ESP32C6
-  initSOCSensor();                    // Initialize analog reading for the Liion battery voltage for SoC calculation
-  initDS18B20();                      // Initialize external DS18B20 temperature sensors
-  initHX711();                        // Initialize HX711 scale IC
-  initBinarySensor();                 // initialize Binary Status from Solar charging IC (Charging + Done) and allow sleep bit from Home Assistant
+  // Create storage for permanent data (still avaliable after resets)
+  preferences.begin("NVM", false);
+  // Initialize internal temperature reading of the ESP32C6
+  initInternalTempSensor();
+  // Initialize analog reading for the Liion battery voltage for SoC calculation
+  initSOCSensor();
+  // Initialize external DS18B20 temperature sensors
+  initDS18B20();                      
+  // Initialize HX711 scale IC
+  initHX711();
+  // initialize Binary Status e.g. for Solar charging IC (Charging + Done)
+  initBinarySensor();
+  // initialize Deep Sleep
+  initDeepSleep();
 
   Serial.println("Starting Zigbee...");
   // When all EPs are registered, start Zigbee in End Device mode
   if (!Zigbee.begin()) {
     Serial.println("Zigbee failed to start!");
-    Serial.println("Going back to sleep...");
-    esp_zb_sleep_enable(true);
-    esp_zb_sleep_set_threshold(3000);
-    esp_zb_set_rx_on_when_idle(false);
-    Serial.flush();
-    esp_sleep_enable_timer_wakeup(10ULL * 1000000ULL);
     Serial.println("Entering Deep Sleep");
     esp_deep_sleep_start();
   } else {
     Serial.println("Zigbee started successfully!");
   }
   Serial.println("Connecting to network");
+  int connectionDuration = 0;
   while (!Zigbee.connected()) {
     Serial.print(".");
     delay(100);
+    connectionDuration++;
   }
   Serial.println();
+  if (connectionDuration > 10) {
+    Serial.println("Initial Connection Detected");
+  }
 }
 
 // Loop is never used due to deep sleep. All function calls are sequential.
@@ -369,10 +389,12 @@ void loop() {
   if (digitalRead(BOOT_PIN) == LOW) {  // Push button pressed
     // Key debounce handling
     delay(100);
+    if (digitalRead(BOOT_PIN) == LOW) {
+    }
     int startTime = millis();
     while (digitalRead(BOOT_PIN) == LOW) {
       delay(50);
-      if ((millis() - startTime) > 3000) {
+      if ((millis() - startTime) > 15000) {
         // If key pressed for more than 3secs, factory reset Zigbee and reboot
         Serial.println("Resetting Zigbee to factory and rebooting in 1s.");
         delay(1000);
@@ -387,19 +409,9 @@ void loop() {
   hx711_sensor_value_update();
   cn3791_status_update();
   delay(2000);
+  enterDeepSleep();
 
-  if (deepSleep) {
-    Serial.println("Stopping Zigbee");
-    Zigbee.stop();
-    esp_zb_sleep_enable(true);
-    esp_zb_sleep_set_threshold(3000);
-    esp_zb_set_rx_on_when_idle(false);
-    Serial.flush();
-    esp_sleep_enable_timer_wakeup(600ULL * 1000000ULL);
-    Serial.println("Entering Deep Sleep");
-    esp_deep_sleep_start();
-  }
-
+  // 10 sec delay in case no deep sleep is defined
   delay(9000);
   hx711.power_up();
   delay(1000);
