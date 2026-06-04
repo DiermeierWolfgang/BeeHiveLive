37c083608f0b4340472b1ce4bc8cccb42cacbb13
Wolfgang Diermeier <wolfgang1.diermeier@st.oth-regensburg.de>
2026-06-04 15:26:24 +0200

    Added Binary Sensors

--- ESP32C6_ArduinoIDE/BeeHiveLive/BeeHiveLive.ino
+++ ESP32C6_ArduinoIDE/BeeHiveLive/BeeHiveLive.ino
@@ -10,11 +10,13 @@
 
 // Input/Output definition
 #define BATTERY_VOLTAGE_PIN A2
-#define I2C_CLK             D5
-#define I2C_DATA            D4
-#define ONE_WIRE_BUS        I2C_DATA
-#define HX711_SCK           D0
-#define HX711_DOUT          D1
+#define I2C_CLK_PIN         D5
+#define I2C_DATA_PIN        D4
+#define ONE_WIRE_BUS_PIN    I2C_DATA_PIN
+#define HX711_SCK_PIN       D0
+#define HX711_DOUT_PIN      D1
+#define CN3791_CHRG_PIN     D3
+#define CN3791_DONE_PIN     D6
 
 // Zigbee endpoint sensor configuration
 #define INTERNAL_TEMP_SENSOR_ENDPOINT_NUMBER  1
@@ -22,7 +24,8 @@
 #define DS18B20_TEMP_SENSOR_2_ENDPOINT_NUMBER 3
 #define DS18B20_TEMP_SENSOR_3_ENDPOINT_NUMBER 4
 #define HX711_SENSOR_ENDPOINT_NUMBER          5
-//#define SOC_SENSOR_ENDPOINT_NUMBER            6
+#define CN3791_CHRG_ENDPOINT_NUMBER           6
+#define CN3791_DONE_ENDPOINT_NUMBER           7
 
 // 1-Wire definitions
 #define ONEWIRE_CMD_MATCH_ROM     0x55
@@ -40,49 +43,50 @@ static const OneWireNg::Id DS18B20[] = {
   { 0x28, 0x6D, 0xE1, 0xC2, 0x63, 0x20, 0x01, 0x60 }
 };
 
+// Get the overall amount of connected DS18B20 temperature sensors
 static const int DS18B20_AMOUNT = sizeof(DS18B20) / sizeof(DS18B20[0]);
 
+// Optional: Define names for the temperature sensors
 static const char* DS18B20_NAME[] = {
     "BeeHive_1",
     "BeeHive_2",
     "BeeHive_3",
 };
 
+
 ZigbeeTempSensor zbInternalTempSensor = ZigbeeTempSensor(INTERNAL_TEMP_SENSOR_ENDPOINT_NUMBER);
-//ZigbeeAnalog zbSocSensor = ZigbeeAnalog(SOC_SENSOR_ENDPOINT_NUMBER);
 ZigbeeTempSensor zbDS18B20TempSensor[DS18B20_AMOUNT] = {
   ZigbeeTempSensor(DS18B20_TEMP_SENSOR_1_ENDPOINT_NUMBER),
   ZigbeeTempSensor(DS18B20_TEMP_SENSOR_2_ENDPOINT_NUMBER),
   ZigbeeTempSensor(DS18B20_TEMP_SENSOR_3_ENDPOINT_NUMBER)
 };
 ZigbeeAnalog zbHX711Sensor = ZigbeeAnalog(HX711_SENSOR_ENDPOINT_NUMBER);
+ZigbeeBinary zbCN3791Chrg = ZigbeeBinary(CN3791_CHRG_ENDPOINT_NUMBER);
+ZigbeeBinary zbCN3791Done = ZigbeeBinary(CN3791_DONE_ENDPOINT_NUMBER);
 
-OneWireNg_CurrentPlatform oneWire(ONE_WIRE_BUS, true);
+OneWireNg_CurrentPlatform oneWire(ONE_WIRE_BUS_PIN, true);
 HX711 hx711;
 
 
 void initInternalTempSensor() {
-  zbInternalTempSensor.setManufacturerAndModel("Wolfgang Diermeier", "BeeHiveLive");              // Set Zigbee device name and model
+  // Set the name of the zigbee device
+  zbInternalTempSensor.setManufacturerAndModel("Wolfgang Diermeier", "BeeHiveLive");
+  // Define the device is battery powered as information for the power cluster. Required for the Battery SoC information.
   zbInternalTempSensor.setPowerSource(ZB_POWER_SOURCE_BATTERY);
-  zbInternalTempSensor.setMinMaxValue(10, 50);                                                    // Set minimum and maximum temperature measurement value (10-50°C is default range for chip temperature measurement)
-  zbInternalTempSensor.setDefaultValue(10.0);                                                     // Optional: Set default (initial) value for the temperature sensor to 10.0°C to match the minimum temperature measurement value
-  Zigbee.addEndpoint(&zbInternalTempSensor);                                                      // Register end point
+  // Set minimum and maximum temperature measurement value (10-50°C is default range for chip temperature measurement)
+  zbInternalTempSensor.setMinMaxValue(10, 50);
+  // Optional: Set default (initial) value for the temperature sensor to 10.0°C to match the minimum temperature measurement value
+  zbInternalTempSensor.setDefaultValue(10.0);
+  // Register end point
+  Zigbee.addEndpoint(&zbInternalTempSensor);
 }
 
-void initAnalogSensor() {
-  pinMode(BATTERY_VOLTAGE_PIN, INPUT);                                                            // Configure Pin as input
-  /* ------------- Outdated -----------------
-  zbSocSensor.addAnalogInput();                                                                   // Adding analog input
-  zbSocSensor.setAnalogInputDescription("Battery SoC");
-  zbSocSensor.setAnalogInputApplication(ESP_ZB_ZCL_AI_PERCENTAGE_OTHER);                          // Define senor reading as percentage value
-  zbSocSensor.setAnalogInputResolution(0.1);
-  zbSocSensor.setAnalogInputMinMax(0.0, 100.0);
-  Zigbee.addEndpoint(&zbSocSensor); */
+void initSOCSensor() {
+  pinMode(BATTERY_VOLTAGE_PIN, INPUT);
 }
 
 void initDS18B20() {
   for (int i = 0; i < DS18B20_AMOUNT; i++) {
-    zbDS18B20TempSensor[i].setManufacturerAndModel("Wolfgang Diermeier", DS18B20_NAME[i]);
     // Set minimum and maximum temperature measurement value
     zbDS18B20TempSensor[i].setMinMaxValue(-40, 85);
     // Optional: Set default (initial) value for the temperature sensor to 20.0°C
@@ -93,13 +97,36 @@ void initDS18B20() {
 }
 
 void initHX711() {
-  hx711.begin(HX711_DOUT, HX711_SCK);
-  pinMode(BATTERY_VOLTAGE_PIN, INPUT);                                                            // Configure Pin as input
-  zbHX711Sensor.addAnalogInput();                                                                   // Adding analog input
+  // Set up the HX711 device
+  hx711.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
+  // Create a new analog zigbee endpoint for the HX711 sensor reading
+  zbHX711Sensor.addAnalogInput();
+  // Define the name of the reading (shows up as friendly name in Home Assistant)
   zbHX711Sensor.setAnalogInputDescription("Weight");
-  zbHX711Sensor.setAnalogInputApplication(0xffffff);                          // Define senor reading as percentage value
+  // Set an unidentified Application to prevent units from showing up in Home Assistant (kg is not avaliable)
+  zbHX711Sensor.setAnalogInputApplication(0xffffff);
+  // raw sensor output resolution is 1
   zbHX711Sensor.setAnalogInputResolution(1);
+  // Register end point
   Zigbee.addEndpoint(&zbHX711Sensor);
+}
+
+void initBinarySensor() {
+  // Configure binary gpios as input (No pull-up since this is hardware implemented)
+  pinMode(CN3791_CHRG_PIN, INPUT);
+  pinMode(CN3791_DONE_PIN, INPUT);
+  // Create new binary zigbee endpoints
+  zbCN3791Chrg.addBinaryInput();
+  zbCN3791Done.addBinaryInput();
+  // Set a suited application
+  zbCN3791Chrg.setBinaryInputApplication(0x00000000);
+  zbCN3791Done.setBinaryInputApplication(0x00000000);
+  // Define the name for each binary sensor
+  zbCN3791Chrg.setBinaryInputDescription("Charging");
+  zbCN3791Done.setBinaryInputDescription("Charged");
+  // Register end points
+  Zigbee.addEndpoint(&zbCN3791Chrg);
+  Zigbee.addEndpoint(&zbCN3791Done);
 }
 
 
@@ -122,9 +149,6 @@ void soc_sensor_value_update() {
 
   Serial.printf("Battery ADC=%d Voltage=%.2fV SoC=%.1f%%\r", adc_value, voltage, soc);
   Serial.println();
-
-  //zbSocSensor.setAnalogInput(soc);
-  //zbSocSensor.reportAnalogInput();
 
   // Battery information is placed into first endpoint
   zbInternalTempSensor.setBatteryPercentage((uint8_t) soc);
@@ -207,6 +231,18 @@ void hx711_sensor_value_update() {
   zbHX711Sensor.reportAnalogInput();
 }
 
+void cn3791_status_update() {
+  // Read status of the CN3791 output pins
+  bool cn3791_chrg = digitalRead(CN3791_CHRG_PIN);
+  bool cn3791_done = digitalRead(CN3791_DONE_PIN);
+
+  // Write pin status to the zigbee binary inputs and report via zigbee
+  zbCN3791Chrg.setBinaryInput(cn3791_chrg);
+  zbCN3791Done.setBinaryInput(cn3791_done);
+  zbCN3791Chrg.reportBinaryInput();
+  zbCN3791Done.reportBinaryInput();
+}
+
 
 /********************* Arduino functions **************************/
 void setup() {
@@ -216,9 +252,10 @@ void setup() {
   pinMode(BOOT_PIN, INPUT_PULLUP);
 
   initInternalTempSensor();           // Initialize internal temperature reading of the ESP32C6
-  initAnalogSensor();                 // Initialize analog reading for the Liion battery voltage for SoC calculation
+  initSOCSensor();                 // Initialize analog reading for the Liion battery voltage for SoC calculation
   initDS18B20();                      // Initialize external DS18B20 temperature sensors
   initHX711();                        // Initialize HX711 scale IC
+  initBinarySensor();                 // initialize Binary Status from Solar charging IC (Charging + Done)
 
   Serial.println("Starting Zigbee...");
   // When all EPs are registered, start Zigbee in End Device mode
